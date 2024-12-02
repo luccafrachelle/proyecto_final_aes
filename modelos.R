2ebbb3c11c6a90ae74a5322cc48fc217da38fd24
luccafrachelle <luccafrachelle73@gmail.com>
2024-12-02 09:38:23 -0300

    emprolijando

--- modelos.R
+++ modelos.R
@@ -3,12 +3,11 @@ library(readxl)
 library(doParallel)
 library(finetune)
 library(bonsai)
+library(vip)
+library(DALEXtra)
 
-# Configurar paralelización
-all_cores <- parallel::detectCores(logical = FALSE)
-registerDoParallel(cores = all_cores)
+registerDoParallel(cores = parallel::detectCores(logical = FALSE))
 
-# Cargar y procesar datos
 df <- read_excel("./data/trayectorias.xlsx") %>%
   select(-c(termina, gol, tiro, termina_tiro, participa_golero,
             xG, zona_fin, x_fin, presion, match_id, team.name,
@@ -23,12 +22,10 @@ df_split <- initial_split(df)
 train <- training(df_split)
 test <- testing(df_split)
 
-# Crear receta
 df_recipe <- recipe(exito ~ ., data = train) %>%
   step_normalize(all_numeric_predictors()) %>%
   step_dummy(all_nominal_predictors())
 
-# Especificar modelos
 model_specs <- list(
   lasso = logistic_reg(penalty = tune(), mixture = 1) %>%
     set_engine("glmnet"),
@@ -51,26 +48,19 @@ model_specs <- list(
     set_mode("classification")
 )
 
-# Crear conjunto de workflows
 workflow_set <- workflow_set(
   preproc = list(base_recipe = df_recipe),
-  models = model_specs
-)
+  models = model_specs)
 
-# Definir validación cruzada
 cv_folds <- vfold_cv(train, v = 5, strata = exito)
 
-# Definir métricas deseadas
 metrics <- metric_set(accuracy, roc_auc, sens, yardstick::spec)
 
-# Configurar control
 control <- control_race(
   save_pred = TRUE,
   parallel_over = "everything",
-  save_workflow = TRUE
-)
+  save_workflow = TRUE)
 
-# Ejecutar tuning
 set.seed(123)
 race_results <- workflow_set %>%
   workflow_map(
@@ -78,47 +68,56 @@ race_results <- workflow_set %>%
     seed = 123,
     resamples = cv_folds,
     grid = 20,
-    metrics = metrics,  # Agregar métricas personalizadas
+    metrics = metrics,
     control = control
   )
 
-# Crear carpetas para guardar modelos y resultados
-model_save_path <- "./models"
-tune_save_path <- "./tune_results"
-dir.create(model_save_path, showWarnings = FALSE)
-dir.create(tune_save_path, showWarnings = FALSE)
+dir.create("./models", showWarnings = FALSE)
 
-# Guardar resultados y modelos ajustados
 lapply(
   race_results$wflow_id,
   function(model_id) {
-    # Extraer resultados del tuning
     tuning_results <- race_results %>%
       extract_workflow_set_result(model_id)
     
-    # Guardar resultados del tuning
-    saveRDS(tuning_results, file = file.path(tune_save_path, paste0(model_id, "_tune_results.rds")))
+    saveRDS(tuning_results, file = file.path("./models", paste0(model_id, "_tune_results.rds")))
     
-    # Seleccionar mejores parámetros
     best_params <- tuning_results %>%
       select_best(metric = "roc_auc")
     
-    # Ajustar modelo final
     model <- race_results %>%
       extract_workflow(model_id) %>%
       finalize_workflow(best_params) %>%
       fit(data = train)
     
-    # Guardar modelo ajustado
-    saveRDS(model, file = file.path(model_save_path, paste0(model_id, "_model.rds")))
-    message(paste("Modelo y tuning de", model_id, "guardados correctamente."))
+    saveRDS(model, file = file.path("./models", paste0(model_id, "_model.rds")))
   }
 )
 
-# Guardar datos marcados como train/test
+best_params <- race_results %>%
+  extract_workflow_set_result('base_recipe_random_forest') %>% 
+  select_best(metric = "roc_auc")
+
+random_forest <- race_results %>%
+  extract_workflow('base_recipe_random_forest') %>%
+  finalize_workflow(best_params) %>%
+  fit(data = train)
+
+train$exito_num <- as.numeric(train$exito) - 1
+
+explainer_rf <- explain_tidymodels(
+  model = random_forest,
+  data = train %>% select(-exito, -exito_num), 
+  y = train$exito_num,
+  label = "Random Forest",
+  verbose = FALSE
+)
+
+vip_rf <- model_parts(explainer_rf)
+
+saveRDS(vip_rf, file = "./models/vip_rf.rds")
+
 train <- train %>% mutate(dataset = "train")
 test <- test %>% mutate(dataset = "test")
 df_marked <- bind_rows(train, test)
-write.csv(df_marked, "./data/data.csv", row.names = FALSE)
-
-message("Datos marcados y guardados en './data/data.csv'.")
+write.csv(df_marked, "./models/data.csv", row.names = FALSE)
